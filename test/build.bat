cad98261f7b20473770b0c792b8e3802321786c2
Colby <me@cobeh.com>
2021-02-26 20:17:18 -0800

    things

--- test/build.bat
+++ test/build.bat
@@ -7,6 +7,14 @@ if not exist bin mkdir bin
 
 pushd bin
 
+if exist *.exe del *.exe
+
 call odin build ..\src\ -out:test.exe -collection:newport=..\..\ -define:USE_VULKAN=true -debug
 
+if exist test.exe (
+    if "%1" == "-run" call test.exe
+    if "%1" == "-r" call test.exe
+)
+
 popd
+
